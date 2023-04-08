c2d13d9850c39739094f611723a68e795c7b1ba1
Siddharth verma <990.siddharthverma@gmail.com>
2023-04-08 15:59:37 +0530

    wip

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -50,4 +50,10 @@ contract Dappcord is ERC721 {
      function getChannel(uint256 _id) public view returns (Channel memory) {
         return channels[_id];
      }
+
+
+     function withdraw() public onlyOwner {
+      (bool success, ) = owner.call{value: address(this).balance}("");
+      require(success);
+     }
 }
