9e9fb7aea1193aa592b1f9a3aaf06b86153f8a89
Siddharth verma <990.siddharthverma@gmail.com>
2023-04-09 19:07:49 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
+      <Navigation />  
 
       <main>
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,16 @@ const Navigation = ({ account, setAccount }) => {
 
   return (
     <nav>
+     <div className='nav__brand'>
+      <h1>Dappcord</h1>
+     </div>
 
+     <button 
+     type="button"
+     className="nav__connect"
+     >
+      Connect
+     </button>
     </nav>
   );
 }
